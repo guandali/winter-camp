defaf7db82e3f79fe6280cd0e6e7ffc3eebbc73f
guanda Li <coliguanda@gmail.com>
2016-12-18 23:38:00 -0800

    Add fizzBuzz practice using java

---
+++ Dec/FizzBuzz/fizzBuzz.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.ArrayList;
+public class fizzBuzz {
+    public List<String> fizzBuzz(int n){
+        
+        List <String> results = new ArrayList<String>();
+        for (int i = 1; i <= n; i++ ){
+            if (i % 15 == 0){
+                results.add("FizzBuzz");
+            }
+            else if (i % 5 == 0){
+                results.add("Buzz");
+            }
+            else if (i % 3 == 0){
+                results.add("Fizz");
+            }
+            else{
+                results.add(Integer.toString(i));
+            }
+                
+        }
+        return results;
+                
+    }
+}
