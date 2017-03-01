8a1f56a5ab839e6e01ad0d4875f28d5cee0c493d
guanda Li <coliguanda@gmail.com>
2017-03-01 01:07:28 -0800

    Java DP solution for uniquePaths problem

---
+++ uniquePaths.java
@@ -0,0 +1,19 @@
+public class uniquePaths {
+    public int uniquePaths(int m, int n) {
+    int [][] matrix= new int[m][n];
+    if (n == 1 || m ==1) return 1;
+    for (int i = 0; i <m;i++ ){
+        for (int j = 0; j <n; j++){
+            if (i == 0|| j== 0){
+                matrix[i][j] = 1;
+            }
+            else {
+                matrix[i][j] = matrix[i - 1][j] + matrix[i][j -1];
+            }
+        }
+    }
+    return matrix[m-1][n-1];
+    
+        
+    }
+}
