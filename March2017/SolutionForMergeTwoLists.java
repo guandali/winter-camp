16053aedc41ee03639f115541388ed093b6fdbfe
guandali <coliguanda@gmail.com>
2017-03-17 00:12:42 -0700

    Recursive solution

--- March2017/SolutionForMergeTwoLists.java
+++ March2017/SolutionForMergeTwoLists.java
@@ -8,33 +8,19 @@
  */
 public class SolutionForMergeTwoLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null){ return l2;}
+               if(l1 == null){ return l2;}
         if(l2 == null){ return l1;}
-        ListNode head ;
-        ListNode i = head;
-        while (l1 != null && l2 != null ){
-            if(l1.val > l2.val){
-                head.val = l2.val;
-                l2 = l2.next;
-            }
-            else {
-                head.val = l1.val;
-                l1 = l1.next;
-            }
-            i = i.next; 
-    }
-    if (l1 == null && l2 != null ){
-        i.val = l2.val;
-        l2 = l2.next;
-        i.next = l2;
-    }
-    else  if (l1 != null && l2 == null ){
-        i.val = l1.val;
-        l1 = l1.next;
-        i.next = l1;
-    }
-    return head;
-    
+        ListNode head;
+        if(l1.val < l2.val){
+            head = l1;
+            head.next = mergeTwoLists(l1.next, l2);
+        }
+        else {
+            head = l2;
+            head.next = mergeTwoLists(l1, l2.next);
+        }
+        
+        return head;
 }
 
 }
