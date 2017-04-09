8e4dc2250f4b9d1079a81800011c438bcceb30ef
guandali <coliguanda@gmail.com>
2017-04-08 23:07:47 -0700

    one-pass solution, reduce time complexity from O(n^2) to O(n)

--- Apr/SolutionBestTimetoBuyandSellStock.java
+++ Apr/SolutionBestTimetoBuyandSellStock.java
@@ -1,19 +1,39 @@
 public class SolutionBestTimetoBuyandSellStock {
-    public int maxProfit(int[] prices) {
-        if(prices == null) return 0;
+    // public int maxProfit(int[] prices) {
+    //     if(prices == null) return 0;
+    //     int len = prices.length;
+    //     int max = 0;
+    //     for(int i = 0; i < len; i++ ){
+    //         for(int j = 0; j < i; j ++){
+    //             if (prices[i] - prices[j] > max){
+    //                 max = prices[i] - prices[j];
+    //             }
+    //         }
+    //     }
+        
+    //     return max;
+            
+    //     }
+        public int maxProfit(int[] prices) {
+        //one-pass solution 
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
         int len = prices.length;
-        int max = 0;
-        for(int i = 0; i < len; i++ ){
-            for(int j = 0; j < i; j ++){
-                if (prices[i] - prices[j] > max){
-                    max = prices[i] - prices[j];
+        for(int i = 0; i < len; i++){
+            if(prices[i] < minPrice){
+                minPrice = prices[i];
+            }
+            else{
+                if(prices[i] - minPrice > maxProfit){
+                   maxProfit =  prices[i] - minPrice;
                 }
             }
         }
+        return maxProfit;
         
-        return max;
             
         }
+        
         
  
 }
