6367a02e935ef966ba6f06a33648bb295e26e52c
guandali <coliguanda@gmail.com>
2017-04-22 20:20:09 -0700

    use 2*n space and n time

--- Apr/NumArray.java
+++ Apr/NumArray.java
@@ -1,27 +1,49 @@
+
 public class NumArray {
-    int [][] dp;
+    int [] dp;
+    int [] nums;
 
     public NumArray(int[] nums) {
-        int len = nums.length;
-        dp = new int [len][len];
-        for (int i = 0; i < len; i ++){
-            for(int j = i; j >=0; j --){
-                if(i == j){
-                    dp[j][i] = nums[i];
-                    
-                }
-                else {
-                    dp[j][i] = dp[j + 1][i] + nums[j];
-                }
-            }
+        this.nums = nums;
+        int len =  nums.length;
+        if (len == 0) return;
+        dp = new int[len];
+        dp[0] = nums[0];
+        for(int i = 1; i < len; i++){
+            dp[i] = dp[i-1] + nums[i];
         }
     }
     
     public int sumRange(int i, int j) {
-        return dp[i][j];
+        if(i>j) return -1;
+        return dp[j] - dp[i] + nums[i];
         
     }
 }
+// public class NumArray {
+//     int [][] dp;
+
+//     public NumArray(int[] nums) {
+//         int len = nums.length;
+//         dp = new int [len][len];
+//         for (int i = 0; i < len; i ++){
+//             for(int j = i; j >=0; j --){
+//                 if(i == j){
+//                     dp[j][i] = nums[i];
+                    
+//                 }
+//                 else {
+//                     dp[j][i] = dp[j + 1][i] + nums[j];
+//                 }
+//             }
+//         }
+//     }
+    
+//     public int sumRange(int i, int j) {
+//         return dp[i][j];
+        
+//     }
+// }
 
 /**
  * Your NumArray object will be instantiated and called as such:
