9289a8927a95d6af1299749f62c6df4a79dda79f
guandali <coliguanda@gmail.com>
2017-05-13 23:26:52 -0700

    Lots corner cases to handle, looking for a more elegent solution.

--- May/SolutionstrStr.java
+++ May/SolutionstrStr.java
@@ -1,6 +1,29 @@
 public class SolutionstrStr {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        //return haystack.indexOf(needle);
+        if(needle.length() > haystack.length()){
+            return -1;
+        }
+        if(needle.length()< 1){
+            return 0;
+        }
+        
+        for(int i = 0; i < haystack.length(); i++){
+           if(haystack.charAt(i) == needle.charAt(0)){
+               for( int j = 0; j < needle.length() && i+j <haystack.length(); j++ ){
+                   if(haystack.charAt(j+i) != needle.charAt(j)){
+                       break;
+                   }
+                   else if(j == needle.length()-1){
+                       return i;
+                   }
+                   
+               }
+               
+           }
+        }
+        
+        return -1;
         
     }
 }
