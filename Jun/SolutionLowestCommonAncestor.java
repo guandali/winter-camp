1850369b0adb35be9e55e208297718d705d57561
guanda Li <coliguanda@gmail.com>
2017-06-08 02:50:10 -0700

    Use recursion to find the first node where right and left are not null. Compare the node at every single recursion and return the node matches.

---
+++ Jun/SolutionLowestCommonAncestor.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class SolutionLowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null){
+            return null;
+        }
+        if(root == p) {
+            return p;
+        }
+        if(root == q){
+            return q;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null){
+            return root;
+        }
+        if(left == null|| right != null){
+            return right;
+        }
+         if(right == null|| left != null){
+            return left;
+        }
+        
+        
+        return null;
+        
+        
+    }
+}
