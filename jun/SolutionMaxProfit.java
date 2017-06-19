a6fbab92ba3a40a6ca971c9e878eb1a79d88f131
guandali <coliguanda@gmail.com>
2017-06-18 21:40:42 -0700

    Removed arrays and only need to know about i-1.

--- jun/SolutionMaxProfit.java
+++ jun/SolutionMaxProfit.java
@@ -3,18 +3,21 @@ public class SolutionMaxProfit {
         // Two options: buy,  sell
         // Need to buy first before sell it
         if(prices.length < 1 ) return 0;
-        int[] sell = new int[prices.length];
-        int[] buy = new int[prices.length];
-        sell[0] = 0;
-        buy[0] = -prices[0];
+       // int[] sell = new int[prices.length];
+       // int[] buy = new int[prices.length];
+        int prev_sell;;
+        int prev_buy;
+        int buy = -prices[0];;
+        int sell = 0;
         int max = Integer.MIN_VALUE;
         for(int i = 1; i < prices.length; i++){
-            sell[i] = Math.max(sell[i-1], buy[i-1] +  prices[i]);
-            buy[i] = Math.max(buy[i-1], sell[i-1] - prices[i]);
+            prev_buy = buy;
+            prev_sell = sell;
+            sell = Math.max(prev_sell, prev_buy +  prices[i]);
+            buy = Math.max(prev_buy, prev_sell - prices[i]);
         }
         
-        return sell[prices.length -1];
-        
+        return sell;
         
     }
 }
