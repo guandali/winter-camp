a1b8172c4a1046bf271b0cb2072a24ab3c4d5be6
guandali <coliguanda@gmail.com>
2017-08-01 22:07:09 -0700

    Using one array.

--- Aug/SolutionProductExceptSelf.java
+++ Aug/SolutionProductExceptSelf.java
@@ -1,18 +1,13 @@
 public class SolutionProductExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         int [] ans = new int[nums.length];
-        int [] tmp0 = new int[nums.length];
-        int [] tmp1 = new int[nums.length];
         for(int i = 0, val = 1; i < nums.length; i++){
-            tmp0[i] = val;
+            ans[i] = val;
             val *= nums[i];
         }
         for(int i = nums.length - 1, val = 1; i >= 0; i--){
-            tmp1[i] = val;
+            ans[i] *= val;
             val *= nums[i];
-        }
-        for(int i = 0; i < nums.length; i++){
-            ans[i] = tmp0[i] * tmp1[i];
         }
         return ans;
     }
