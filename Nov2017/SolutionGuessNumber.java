d7d57086f7d652c4488117e5842afdf38c7722d4
guanda Li <coliguanda@gmail.com>
2017-12-01 00:00:06 -0800

    binary search

---
+++ Nov2017/SolutionGuessNumber.java
@@ -0,0 +1,19 @@
+/* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+
+      public class SolutionGuessNumber extends GuessGame {
+        public int guessNumber(int n) {
+            int lo = 1;
+            int hi = n;
+            while(lo <= hi){
+                int mid = (hi - lo)/2 + lo;
+                int result = guess(mid);
+                if(result == 0) return mid;
+                else if(result < 0) hi = mid - 1;
+                else lo = mid + 1;
+            }
+            return -1;
+        }
+    }
